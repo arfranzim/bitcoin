5bbb3f8b54e8cb795451532e7ceda33a1f3aaff8
arfranzim <andrefranzim@gmail.com>
2020-10-15 17:16:23 -0300

    Aplicação configurada pra requisição externa

---
+++ src/main/java/corp/franzim/service/BitcoinService.java
@@ -0,0 +1,21 @@
+package corp.franzim.service;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import corp.franzim.model.Bitcoin;
+
+@Path("/bitcoins")
+@RegisterRestClient
+public interface BitcoinService {
+    
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Bitcoin> listar();
+}
